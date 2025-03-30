b3f7498385decbdc6986b0098578d9cb380255f3
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-03-30 09:03:08 +0700

    feat(cms): change user ID to uuid and add full name field

--- src/collections/Users.ts
+++ src/collections/Users.ts
@@ -3,11 +3,17 @@ import type { CollectionConfig } from 'payload'
 export const Users: CollectionConfig = {
   slug: 'users',
   admin: {
-    useAsTitle: 'email',
+    useAsTitle: 'full-name',
   },
   auth: true,
   fields: [
     // Email added by default
     // Add more fields as needed
+    {
+      name: 'full-name',
+      label: 'Full Name',
+      type: 'text',
+      required: true,
+    },
   ],
 }
--- src/payload.config.ts
+++ src/payload.config.ts
@@ -30,6 +30,7 @@ export default buildConfig({
     pool: {
       connectionString: process.env.DATABASE_URI || '',
     },
+    idType: 'uuid'
   }),
   sharp,
   plugins: [
