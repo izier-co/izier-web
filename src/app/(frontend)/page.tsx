8d00892dcf6dcdd1c181ae4977cbc0c00e3194e3
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-04-06 17:56:21 +0700

    feat: implement HomePage layout with title and value proposition

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,5 +1,18 @@
+import Title from '@/components/heroTitle'
+import ValueProposition from '@/components/valueProp'
 import './globals.css'
 
 export default async function HomePage() {
-  return <div></div>
+  return (
+    <div className="flex flex-col gap-6">
+      <Title text="Smarter, Scalable, Secure Software Built for Business Success" />
+      <ValueProposition>
+        Driven by a vision to empower growth, we enable enterprises to overcome traditional barriers
+        through agile digital integration. At Izier, we deliver flexible, scalable, and secure
+        software designed to solve business problems and optimize performance. Our advanced
+        solutions streamline operations, boost efficiency, and drive innovation, helping businesses
+        thrive in dynamic markets and maintain a competitive edge.
+      </ValueProposition>
+    </div>
+  )
 }
