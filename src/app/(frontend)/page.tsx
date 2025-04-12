54e7b95c4d10f3f7789b9c107d208ffae684f0b4
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-04-12 12:33:06 +0700

    feat(wip): add service page

--- src/app/(frontend)/page.tsx
+++ src/app/(frontend)/page.tsx
@@ -1,8 +1,15 @@
+import './globals.css'
 import Title from '@/components/heroTitle'
 import ValueProposition from '@/components/valueProp'
-import './globals.css'
+import { Service, ServiceList, ServiceText } from '@/components/services'
+import CustomLink from '@/components/customLink'
+import { getPayload } from 'payload'
+import config from '@payload-config'
 
 export default async function HomePage() {
+  const payload = await getPayload({ config })
+  const findResult = await payload.find({ collection: 'services' })
+
   return (
     <div className="flex flex-col gap-6">
       <Title text="Smarter, Scalable, Secure Software Built for Business Success" />
@@ -13,6 +20,22 @@ export default async function HomePage() {
         solutions streamline operations, boost efficiency, and drive innovation, helping businesses
         thrive in dynamic markets and maintain a competitive edge.
       </ValueProposition>
+      <Service>
+        <ServiceText>
+          <CustomLink href="#" target="_self">
+            Izier&apos;s
+          </CustomLink>
+          &nbsp;agile, enterprise-grade solutions are built to scale — delivering secure software
+          that solves real business problems. Whether you&apos;re scaling up or optimizing, our
+          solutions are built to grow with your business, turning technology into a strategic
+          advantage.
+        </ServiceText>
+        <>
+          {findResult.docs.map((page) => {
+            <ServiceList href={page['service-url']}>{page['service-name']}</ServiceList>
+          })}
+        </>
+      </Service>
     </div>
   )
 }
