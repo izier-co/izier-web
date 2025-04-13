a4ed0d3c536e339a2f4d67fb84751ef1e615a279
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-04-13 11:06:56 +0700

    style: add capitalize class to CustomLink component

--- src/app/components/customLink.tsx
+++ src/app/components/customLink.tsx
@@ -11,7 +11,7 @@ const CustomLink = ({ href, children, target }: LinkProps) => {
     <Link
       href={href}
       target={target}
-      className="text-light-green-11 dark:text-dark-green-11 hover:text-light-green-12 dark:hover:text-dark-green-12 hover:underline visited:text-ligth-green-5"
+      className="text-light-green-11 capitalize dark:text-dark-green-11 hover:text-light-green-12 dark:hover:text-dark-green-12 hover:underline visited:text-ligth-green-5"
     >
       {children}
     </Link>
