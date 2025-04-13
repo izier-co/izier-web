26f39ea7cf2286fcd4f17500a9a42b28f1b3ef81
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-04-13 11:19:02 +0700

    style: adjust layout of ServiceList component for better responsiveness

--- src/app/components/services.tsx
+++ src/app/components/services.tsx
@@ -24,8 +24,8 @@ export const ServiceText = ({ children }: ServiceProps) => {
 export const ServiceList = ({ href, children }: ServiceListProps) => {
   return (
     <div>
-      <ul className="grid grid-cols-2 grid-rows-3 gap-x-12 gap-y-4 w-fit list-inside list-disc list-image-[url('/lists-icon-light.svg')] dark:list-image-[url('/lists-icon-dark.svg')] align-middle">
-        <li className="w-fit">
+      <ul className="h-fit w-fit list-inside list-disc list-image-[url('/lists-icon-light.svg')] dark:list-image-[url('/lists-icon-dark.svg')] align-middle">
+        <li className="w-fit h-fit">
           <CustomLink href={href} target="_self">
             {children}
           </CustomLink>
