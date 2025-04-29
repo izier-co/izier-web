a51d5cbb0be5488f233f5fb28ef11ebe5f80057f
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-04-29 20:40:34 +0700

    feat(payload): add S3 storage configuration

--- src/payload.config.ts
+++ src/payload.config.ts
@@ -10,6 +10,7 @@ import sharp from 'sharp'
 import { Users } from './collections/Users'
 import { Media } from './collections/Media'
 import { ServiceList } from './collections/ServiceList'
+import { s3Storage } from '@payloadcms/storage-s3'
 
 const filename = fileURLToPath(import.meta.url)
 const dirname = path.dirname(filename)
@@ -36,6 +37,25 @@ export default buildConfig({
   sharp,
   plugins: [
     payloadCloudPlugin(),
-    // storage-adapter-placeholder
+    s3Storage({
+      bucket: process.env.R2_BUCKET || '',
+      disableLocalStorage: true,
+      config: {
+        credentials: {
+          accessKeyId: process.env.R2_ACCESS_KEY_ID || '',
+          secretAccessKey: process.env.R2_SECRET_ACCESS_KEY || '',
+          accountId: process.env.R2_ACCOUNT_ID || '',
+        },
+        endpoint: process.env.R2_ENDPOINT || '',
+        region: 'auto',
+        forcePathStyle: true,
+      },
+      collections: {
+        media: {
+          disableLocalStorage: true,
+          prefix: 'media',
+        },
+      },
+    }),
   ],
 })
