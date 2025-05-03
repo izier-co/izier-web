4addf277192bfd29de5f2156b1669b9d5dcab7b2
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-05-03 23:26:27 +0700

    fix(collection): remove unique constraint from 'useAs' field in Media collection

--- src/collections/Media.ts
+++ src/collections/Media.ts
@@ -28,7 +28,6 @@ export const Media: CollectionConfig = {
         { label: 'Test Image', value: 'test-image' },
       ],
       hasMany: false,
-      unique: true,
       defaultValue: 'global-image',
     },
     {
