851ded48f75fcae845439005f43fad41c1d2f453
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-06-08 12:04:17 +0700

    feat(wip): add workflow component

---
+++ src/app/components/workflow.tsx
@@ -0,0 +1,15 @@
+import CustomLink from './customLink'
+
+const Workflow = () => {
+  return (
+    <div>
+      <CustomLink href="#methodology">
+        <h2 id="methodology" className="underline">
+          Workflow - From Idea to Impact
+        </h2>
+      </CustomLink>
+    </div>
+  )
+}
+
+export default Workflow
