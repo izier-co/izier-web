584460aee70cc18e309cf7038707dc5a44aca374
Andrew Virya Victorio <andrew.avv03@gmail.com>
2025-06-08 08:23:13 +0700

    feat(comps): add custom divider

---
+++ src/app/components/customDivider.tsx
@@ -0,0 +1,9 @@
+const CustomDivider = () => {
+  return (
+    <div>
+      <hr className="border-light-green-10 dark:border-dark-green-10 border-dashed w-full" />
+    </div>
+  )
+}
+
+export default CustomDivider
